b07e1100336fb1f17dfed025b94c3dc085791434
NWelde <131807435+NWelde@users.noreply.github.com>
2024-01-10 21:56:43 +0300

    all bugs fixed

--- counter program/index.js
+++ counter program/index.js
@@ -4,15 +4,17 @@ const res = document.getElementById("res");
 const inc = document.getElementById("inc");
 const countlabel = document.getElementById("lab");
 
-inc.addEventListener = function(){
+inc.addEventListener("click", function() {
     count++;
     countlabel.textContent = count;
-}
-dec.addEventListener = function(){
+});
+
+dec.addEventListener("click", function() {
     count--;
     countlabel.textContent = count;
-}
-res.addEventListener = function(){
-    count=0;
+});
+
+res.addEventListener("click", function() {
+    count = 0;
     countlabel.textContent = count;
-}
+})
