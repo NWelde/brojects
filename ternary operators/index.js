b4b0c33f02ef8efbadb8f7e7c2ba4baeab30f607
NWelde <131807435+NWelde@users.noreply.github.com>
2024-01-12 17:43:53 +0300

    ternary operators
    practice file check it for future refrences on ternary operators

---
+++ ternary operators/index.js
@@ -0,0 +1,4 @@
+let age = 21;
+let message = age >= 18 ? "you are an adult" : "you are a minor ";  /* a ternary operator is just like a if statment but its easier to write is is only useable in samll conditions what its doing
+here is if age>=18 == true it assigns the first message to the variable but if the condition is false it assigns the second one and the way its structured is let x = x>5 ? "true":"false"//**** */
+
