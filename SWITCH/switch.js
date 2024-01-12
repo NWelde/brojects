7d6f72b3a49294fa2e3ce2e1dac6060ac8396dea
NWelde <131807435+NWelde@users.noreply.github.com>
2024-01-12 20:25:52 +0300

    Create switch.js
    make sure to finish this and apply this topic in a future topic

---
+++ SWITCH/switch.js
@@ -0,0 +1,22 @@
+let day = 1;
+switch(day){
+    case 1:
+        console.log("today is monday");
+        break;
+    case 2:
+        console.log( "today is tuesday ");
+        break;
+    case 3:
+        console.log( "today is wednesday");
+        break;
+    case 4:
+        console.log( "today is thursday ");             
+    case 5:
+        console.log("today is friday");
+        break;
+     case 6:
+        return "saturday "
+    case 7:
+        return "sunday"
+}
+// finish this whever you have the time very imprtant topic
