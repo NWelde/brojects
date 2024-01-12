223223014219bb70cb400c262bfbaa6522388be1
NWelde <131807435+NWelde@users.noreply.github.com>
2024-01-12 20:53:44 +0300

    Create string methods and slicing.js
    random practice file

---
+++ string methods/string methods and slicing.js
@@ -0,0 +1,25 @@
+let username = "name" ;
+
+//console.log(username.charAt(0))*/
+
+//console.log(username.indexOf(3))
+
+/*userName = username.trim();
+
+userName = username.toUpperCase();
+
+username.toLowerCase /* */
+
+username.startsWith("") 
+
+
+
+
+
+//string slicing is creating another string from a parent string string.slice(start,end)
+
+const fullName = "nathan";
+
+let firstname = fullName.slice(0,2);
+
+console.log(firstname)
