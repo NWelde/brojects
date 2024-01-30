cc089eccb30db3b45c2ff82d59bc737c69280973
NWelde <131807435+NWelde@users.noreply.github.com>
2024-01-30 21:37:51 +0300

    Create maps.js
    learned the usecase of maps

---
+++ maps.js
@@ -0,0 +1,7 @@
+let num = [1, 2, 3, 4, 5];
+
+const incrementedNum = num.map(number => number + 1); // Create a new array with incremented values
+
+console.log(incrementedNum); // Output: [2, 3, 4, 5, 6]
+
+//the only diffrence between foreach and maps is that maps creates a new array
