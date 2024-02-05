67c38b67bff81ec1a60e5bc5ac9fa13ec433ed2f
NWelde <131807435+NWelde@users.noreply.github.com>
2024-02-05 14:19:35 +0300

    Create 1 to 1000.js

---
+++ 1 to 1000.js
@@ -0,0 +1,7 @@
+function sumDigits1() { //the main function of the program
+    let sum = 0;  //inisalizes the sum variable 
+    for (let i = 1; i <= 1000; i++) { //initalizes the i variable stores numbers from 1 to 1000 and stores it in sum 
+      sum += i; //adds the inital value of i to the value stored in sum
+    }
+    return sum;
+  }
